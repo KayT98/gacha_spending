de443c25fe60514142ab8c204ae2d16f1898e74d
kay <adachi3690@gmail.com>
2026-05-02 23:10:41 -0500

    Update frequently purchased.sql

--- frequently purchased.sql
+++ frequently purchased.sql
@@ -1,7 +1,12 @@
-SELECT Items, COUNT(*) AS time_purchased, ROUND(SUM(Price), 2) AS Total
+
+-- This query shows all items I have purchased **more than once**, along with:
+-- How many times I bought each item
+-- Total amount spent on each item
+
+SELECT Items, COUNT(*) AS time_purchased, ROUND(SUM(Price), 2) AS Total -- Adds up the total money spent on this item (rounded to 2 decimals)
 FROM games
-GROUP BY Items
-HAVING COUNT(*) > 1
+GROUP BY Items -- Groups the data by each unique item
+HAVING COUNT(*) > 1 -- Filter to show only items bought multiple times (exclude single purchases)
 ORDER BY Items DESC;
 
 

5044a24fe64a003dc34dfde56cd6c48516bf5846
kay <71146091+KayT98@users.noreply.github.com>
2026-05-03 23:29:58 -0500

    Update frequently and one time purchases.sql

--- frequently and one time purchases.sql
+++ frequently and one time purchases.sql
@@ -1,12 +1,22 @@
-
--- This query shows all items I have purchased one time, along with:
+-- This query shows all items I have purchased more than once, along with:
 -- How many times I bought each item
 -- Total amount spent on each item
 
-SELECT Game, Items, COUNT(*) AS time_purchased, SUM(Price) AS Total -- Adds up the total money spent on this item (rounded to 2 decimals)
+SELECT Game, Items, COUNT(*) AS time_purchased, ROUND(SUM(Price), 2) AS Total -- Adds up the total money spent on this item (rounded to 2 decimals)
 FROM games
-GROUP BY Game, Items -- Groups the data by each unique item
-HAVING COUNT(*) = 1 -- Single purchases only
+GROUP BY Game, Items -- Groups the data by each unique item and game
+HAVING COUNT(*) > 1 -- Filter to show only items bought multiple times (exclude single purchases)
+ORDER BY Game, Items DESC;
+
+--------------------------------------------------------------------------------------------
+
+-- This query shows all items I have purchased only once, along with:
+-- Total amount spent on each item
+
+SELECT Game, Items, COUNT(*) AS time_purchased, SUM(Price) AS Total
+FROM games
+GROUP BY Game, Items -- Groups the data by each unique item and game
+HAVING COUNT(*) = 1 -- One time purchases only
 ORDER BY Game, Items DESC;
 
 
